c247ea8e7ef5b1a6bd199bed20b2a357e3db93b3
李胜国 <lsg@rrtimes.com>
2018-12-07 15:59:45 +0800

    maka-html-view

---
+++ packages/example/maka-html-view/index.js
@@ -0,0 +1,32 @@
+import pkgJson from './package.json'
+import { actionMixin } from 'maka'
+import view from './view.html'
+import './style.less'
+
+const name = pkgJson.name
+
+const state = {
+    data: {
+        content: 'hello ',
+        input: ''
+    }
+}
+
+@actionMixin('base')
+class action {
+    constructor(option) {
+        Object.assign(this, option.mixins)
+    }
+
+    onChange = (e) => {
+        this.base.setState({ 'data.input': e.target.value })
+        console.log(this.base.getState('data.input'))
+    }
+}
+ 
+export {
+    name,
+    state,
+    action,
+    view
+}
