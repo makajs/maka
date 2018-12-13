af8cdff7e4854d1e3b00a6036b4eb0203038358e
ziaochina@gmail.com <ziaochina@gmail.com>
2018-12-13 13:34:40 +0800

    When exec build --dev, webpack mode=development

--- packages/maka-cli/config/webpack.config.js
+++ packages/maka-cli/config/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = function (option) {
     }
 
     return {
-        mode: 'production',
+        mode: isProd ? 'production': 'development',
         ...ext,
         optimization: {
             minimizer
