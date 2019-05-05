99c06c98d06940cd1afaf624dfec8022c4c9d99f
ziaochina <ziaochina@gmail.com>
2019-05-05 15:16:46 +0800

    modify koa server

--- packages/maka-koa-server/app.js
+++ packages/maka-koa-server/app.js
@@ -51,7 +51,7 @@ function getPcContent() {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>APaaS</title>
+    <title>KOA</title>
     <link rel="shortcut icon" href="${cdn}/_assets/v${version}/favicon.ico">
     <style>
         html,
@@ -102,7 +102,7 @@ function getMobileContent(){
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1, width=device-width, height=device-height, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>APaaS Mobile</title>
+    <title>Mobile</title>
     <link rel="shortcut icon" href="${cdn}/_assets/v${version}/favicon.ico">
     <style>
         html,body,#app {
@@ -122,11 +122,6 @@ function getMobileContent(){
             require.onError = function (err) {
                 debugger
             };
-            var excludeMockUrls = [
-                /bpm\\/.*/,
-                /apaas\\/.*/,
-                /api-1\\/.*/
-            ]
             maka.utils.fetch.config({
                 mock: true,
                 excludeMockUrls: excludeMockUrls,
